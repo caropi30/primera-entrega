dd92543c5d1b364ff44445da43f41c42494473a4
unknown <caropi30@gmail.com>
2024-01-18 21:17:15 -0300

    feat: nueva integracion componente primerComponente

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PrimerComponente from './src/components/PrimerComponente';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <PrimerComponente />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#CC4BC2',
     alignItems: 'center',
     justifyContent: 'center',
   },
